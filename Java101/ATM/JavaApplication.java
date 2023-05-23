3ee7202cb2396ffd7c3a4f95c38d8ead0fa986d4
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-05-23 23:56:40 +0300

    ATM eklendi

---
+++ Java101/ATM/JavaApplication.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author yusuf
+ */
+import java.util.Scanner;
+
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+     String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız :");
+            userName = input.nextLine();
+            System.out.print("Parolanız : ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do {
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = input.nextInt();
+                    switch(select){
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            int price = input.nextInt();
+                            balance += price;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            int price1 = input.nextInt();
+                            if (price1 > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                             balance -= price1;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                    }
+                
+                } while (select != 4);
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+    }
+}
