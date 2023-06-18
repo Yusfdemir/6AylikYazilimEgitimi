f434dab511b951cf0680dc8caba3dbe865ccc437
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-06-18 16:09:27 +0300

    Student Information Ekledni

---
+++ Java101/StudentInformation/Teacher.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author yusuf
+ */
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
