ab8e2cbd7bbf99fd3408b0fbecf728af273827fc
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-07-02 22:36:32 +0300

    Palindrome Eklendi

---
+++ Java101/Palindrome/JavaApplication.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author yusuf
+ */
+
+public class JavaApplication {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("kavk"));
+      
+    }
+    
+  } 
+
